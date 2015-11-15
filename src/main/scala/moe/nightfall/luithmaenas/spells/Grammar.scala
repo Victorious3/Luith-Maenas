fa49ff5b8d0e783601fb97949c9cbc2c663222a8
Victorious3 <vic@nightfall.moe>
2015-11-15 12:19:54 +0100

    Fiddled around with tokens a bit

--- src/main/scala/moe/nightfall/luithmaenas/spells/Grammar.scala
+++ src/main/scala/moe/nightfall/luithmaenas/spells/Grammar.scala
@@ -3,6 +3,8 @@ package moe.nightfall.luithmaenas.spells
 import moe.nightfall.luithmaenas.POS.Category
 import moe.nightfall.luithmaenas.POS
 import collection.mutable
+import moe.nightfall.luithmaenas.spells.TokenFactory.WordFactory
+import moe.nightfall.luithmaenas.spells.TokenFactory.SentenceFactory
 
 /**
  * @author "Vic Nightfall"
@@ -12,18 +14,20 @@ object Grammar {
     val factories = mutable.Map[Category, TokenFactory[_]]()
     
     // Predefined Factories
-    val nnFactory = new TokenFactory.Word(default = (s, v) => new Entity(s, v))
-    val vbFactory = new TokenFactory.Word(default = (s, v) => new Action(s, v))
-    val jjFactory = new TokenFactory.Word(default = (s, v) => new Property(s, v))
+    val nnFactory = new WordFactory(default = (s, v) => new Entity(s, v))
+    val vbFactory = new WordFactory(default = (s, v) => new Action(s, v))
+    val jjFactory = new WordFactory(default = (s, v) => new Property(s, v))
     
     def init() {
         newFactory(POS.noun, nnFactory)
         newFactory(POS.verb, vbFactory)
         newFactory(POS.adjective, jjFactory)
+        
+        newFactory('S, SentenceFactory)
     }
     
-    def factory[T >: Token](ct: Category) : Option[TokenFactory[T]] = factories.get(ct).asInstanceOf[Option[TokenFactory[T]]]
-    def newFactory[T >: Token](ct: Category, tf: TokenFactory[T]) = factories += ct -> tf
+    def factory[T <: Token](ct: Category) : Option[TokenFactory[T]] = factories.get(ct).asInstanceOf[Option[TokenFactory[T]]]
+    def newFactory[T <: Token](ct: Category, tf: TokenFactory[T]) = factories += ct -> tf
     
     
 }
--- src/main/scala/moe/nightfall/luithmaenas/spells/Token.scala
+++ src/main/scala/moe/nightfall/luithmaenas/spells/Token.scala
@@ -5,6 +5,7 @@ import moe.nightfall.luithmaenas.POS
 import edu.stanford.nlp.trees.Tree
 import scala.collection.mutable.ListBuffer
 import scala.collection.concurrent.TrieMap
+import scala.reflect.internal.Symbols
 
 /**
  * @author "Vic Nightfall"
@@ -14,47 +15,81 @@ trait Token {
     val children: ListBuffer[Token] = new ListBuffer
     val symbol: Symbol
     
-    override def equals(other: Any) = other match {
-        case other: Token => other.symbol == this.symbol
+    /** Contains the factory that was used to construct this token */
+    var factory: TokenFactory[this.type] = _
+    /** Contains the parent token */
+    var parent: Token = _
+    
+    final override def equals(other: Any) = other match {
+        case other: this.type => other.symbol == this.symbol
         case _ => false
     }
     
-    override lazy val hashCode = symbol.hashCode
+    final override lazy val hashCode = symbol.hashCode
     
-    def isLeaf = children.isEmpty
+    final def isLeaf = children.isEmpty
 }
 
-trait TokenFactory[T >: Token] {
+/**
+ * `TokenFactory` is used to create `Token`. 
+ * You can add listeners that allow you to modify existing tokens easily.
+ */
+trait TokenFactory[T <: Token] {
+    
+    /** List of pre-converters, use this if you need more control over the order */
+    val before = ListBuffer[T => T]()
+    /** List of post-converters, use this if you need more control over the order */
+    val after = ListBuffer[T => T]()
+    
     def create(tree: Tree) : T
+    
+    /** Adds a new listener to the token factory that gets invoked upon walking the
+     *  tree inwards, e.g before constructing the children
+     */
+    def before(converter: T => T) {
+        before += converter
+    }
+    
+    /** adds a new listener to the token factory that gets invoked upon walking the
+     *  tree outwards, e.g after constructing the children
+     */
+    def after(converter: T => T) {
+        after += converter
+    }
 }
 
 object TokenFactory {
-    class Word[T >: Token](val default: (Symbol, Tree) => T) extends TokenFactory[T] {
-      val tokens: TrieMap[Symbol, (Symbol, Tree) => T] = new TrieMap
-      
-      override def create(tree: Tree) : T = {
-          val symbol = POS.toSymbol(tree.firstChild.label)
-          tokens.get(symbol).getOrElse(default).apply(symbol, tree)
-      }
-      
-      def ++= (factory: TokenFactory.Word[T]) : TokenFactory.Word[T] = {
-          tokens ++= factory.tokens
-          return this
-      }
-  
-      def += (kv : (Symbol, (Symbol, Tree) => T)) : TokenFactory.Word[T] = {
-          tokens += kv
-          return this
-      }
-  }
+    class WordFactory[T <: Token](val default: (Symbol, Tree) => T) extends TokenFactory[T] {
+        val tokens: TrieMap[Symbol, (Symbol, Tree) => T] = new TrieMap
+        
+        override def create(tree: Tree) : T = {
+            val symbol = POS.toSymbol(tree.firstChild.label)
+            tokens.get(symbol).getOrElse(default).apply(symbol, tree)
+        }
+        
+        def ++= (factory: WordFactory[T]) : WordFactory[T] = {
+            tokens ++= factory.tokens
+            return this
+        }
+    
+        def += (kv : (Symbol, (Symbol, Tree) => T)) : WordFactory[T] = {
+            tokens += kv
+            return this
+        }
+    }
+    
+    object SentenceFactory extends TokenFactory[Sentence] {
+        override def create(tree: Tree) : Sentence = new Sentence(tree)
+    }
 }
 
-
+class Sentence(val tree: Tree) extends Token {
+    override val symbol = Symbol("")
+}
 
 class Entity(val symbol: Symbol, tree: Tree) extends Token
 
 class Property(val symbol: Symbol, tree: Tree) extends Token
-
 
 trait Combineable extends Property {
     def += (property: this.type): this.type = this
